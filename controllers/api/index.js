f43bd782e38164113e60ec05a55e298524a3c61b
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-16 21:16:10 -0800

    setting up API Index.js routes to handle Game and Review routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const gameRoutes = require('./gameRoutes');
+const reviewRoutes = require('./reviewRoutes');
 
 router.use('/users', userRoutes);
+router.use('/review', reviewRoutes);
+router.use('/game', gameRoutes);
 
 module.exports = router;
