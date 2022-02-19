09c965d79fa4963cd2b4743bce1cbc42a9782ba6
marvanessa1 <marvanessa1@live.com>
2022-02-19 13:11:22 -0800

    added public folders, and modified the following handlebars: main, login, profile, homepage

---
+++ public/js/carousel.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
