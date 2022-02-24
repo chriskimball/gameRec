fb76d7b9b31f3c41014c49fa9b4e1f339c6a5425
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-24 00:41:01 -0800

    Working POST API route for review

--- public/js/review.js
+++ public/js/review.js
@@ -3,22 +3,22 @@ const reviewFormHandler = async (event) => {
 
   const gameId = document.querySelector('#game-title').value;
   const platform = document.querySelector('#platform').value;
-  const review = document.querySelector('#review').value.trim();
+  const review_text = document.querySelector('#review').value.trim();
 
   // Accessing radio rating value without jQuery
   const ratingRadios = document.querySelectorAll('input[type="radio"]:checked').values();
   const values = Array.from(ratingRadios, (radio) => radio.value);
   rating = values[0];
 
-  if (gameId && platform && rating && review) {
+  if (gameId && platform && rating && review_text) {
     const response = await fetch(`/api/review/${gameId}`, {
       method: 'POST',
-      body: JSON.stringify({ gameId, rating, platform, review }),
+      body: JSON.stringify({ gameId, rating, platform, review_text }),
       headers: { 'Content-Type': 'application/json' },
     });
 
     if (response.ok) {
-      document.location.replace('/profile');
+      document.location.replace(`/games/${gameId}`);
     } else {
       alert(response.statusText);
     }
