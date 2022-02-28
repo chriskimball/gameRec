22fd3754ed040b4a130f89a4ad3e4d27308d220d
chriskimball <92878250+chriskimball@users.noreply.github.com>
2022-02-28 01:05:01 -0800

    Removed public/js/profile.js as it was copied from external project and was not needed for our profile. Also added my picture/recommended games to about-us page

--- public/js/search-bar.js
+++ public/js/search-bar.js
@@ -4,7 +4,7 @@ $(function () {
       value: 'https://gamrec/project.herokuapp.com/games/1',
       label: 'Grand Theft Auto V',
     },
-    { value: '/games/2', label: 'The Witcher 3: Wildl Hunt' },
+    { value: '/games/2', label: 'The Witcher 3: Wild Hunt' },
     { value: '/games/3', label: 'Portal 2' },
     { value: '/games/4', label: 'The Elder Scrolls V: Skyrim' },
     { value: '/games/5', label: 'Counter-Strike: Global Offensive' },
